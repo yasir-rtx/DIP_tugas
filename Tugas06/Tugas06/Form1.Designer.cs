aaa224772481f37ff1106b1b1c9e542fa4191f44
yasir-rtx <yelfvankarlenstain@gmail.com>
2021-05-23 21:45:20 +0700

    Tugas06 All Working 100%

---
+++ Tugas06/Tugas06/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Tugas06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(418, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Grayscale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Filter 4 Node";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(250, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Filter 8 Node";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(332, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(843, 281);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(424, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(411, 254);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 359);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Tugas06/Tugas06/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas06
+{
+    public partial class Form1 : Form
+    {
+        Bitmap objek;
+        Bitmap obj;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult d = openFileDialog1.ShowDialog();
+            if (d == DialogResult.OK)
+            {
+                obj = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = obj;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            objek = new Bitmap(obj);
+            for (int x = 0; x < objek.Width; x++)
+            {
+                for (int y = 0; y < objek.Height; y++)
+                {
+                    Color color = objek.GetPixel(x, y);
+                    int xg = (int)((color.R + color.B + color.G) / 3);
+                    Color newColor = Color.FromArgb(xg, xg, xg);
+                    objek.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox2.Image = objek;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float[] a = new float[5];
+            a[0] = (float)-0.5;
+            a[1] = (float)-0.5;
+            a[2] = (float)1;
+            a[3] = (float)0.5;
+            a[4] = (float)0.5;
+
+            Bitmap objek4;
+            objek4 = new Bitmap(objek);
+
+            for (int x = 1; x < objek4.Width - 1; x++)
+            {
+                for (int y = 1; y < objek4.Height - 1; y++)
+                {
+                    // Read Color 4 Matrix
+                    Color c0 = objek4.GetPixel(x, y - 1);
+                    Color c1 = objek4.GetPixel(x - 1, y);
+                    Color c2 = objek4.GetPixel(x, y);
+                    Color c3 = objek4.GetPixel(x + 1, y);
+                    Color c4 = objek4.GetPixel(x, y + 1);
+
+                    // Grayscale c0...c4
+                    int x0 = (int)((c0.R + c0.G + c0.B) / 3);
+                    int x1 = (int)((c1.R + c1.G + c1.B) / 3);
+                    int x2 = (int)((c2.R + c2.G + c2.B) / 3);
+                    int x3 = (int)((c3.R + c3.G + c3.B) / 3);
+                    int x4 = (int)((c4.R + c4.G + c4.B) / 3);
+
+                    // Initiate 4 Matrix Filtering
+                    int xn = (int)((a[0] * x0) + (a[1] * x1) + (a[2] * x2) + (a[3] * x3) + (a[4] * x4));
+                    if (xn < 0) xn = 0;
+                    if (xn > 255) xn = 255;
+
+                    Color newColor = Color.FromArgb(xn, xn, xn);
+                    objek4.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox2.Image = objek4;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float[] a = new float[10];
+            a[1] = (float)-1;
+            a[2] = (float)-0.5;
+            a[3] = (float)0;
+            a[4] = (float)-0.5;
+            a[5] = (float)1;
+            a[6] = (float)0.5;
+            a[7] = (float)0;
+            a[8] = (float)0.5;
+            a[9] = (float)1;
+
+            Bitmap objek8;
+            objek8 = new Bitmap(objek);
+
+            for (int x = 1; x < objek8.Width - 1; x++)
+            {
+                for (int y = 1; y < objek8.Height - 1; y++)
+                {
+                    // Read RGB 4 Matrix
+                    Color c1 = objek8.GetPixel(x - 1, y - 1);
+                    Color c2 = objek8.GetPixel(x, y - 1);
+                    Color c3 = objek8.GetPixel(x + 1, y - 1);
+                    Color c4 = objek8.GetPixel(x - 1, y);
+                    Color c5 = objek8.GetPixel(x, y);
+                    Color c6 = objek8.GetPixel(x + 1, y);
+                    Color c7 = objek8.GetPixel(x - 1, y + 1);
+                    Color c8 = objek8.GetPixel(x, y + 1);
+                    Color c9 = objek8.GetPixel(x + 1, y + 1);
+
+                    // Grayscale 1...9
+                    int x1 = (int)((c1.R + c1.G + c1.B) / 3);
+                    int x2 = (int)((c2.R + c2.G + c2.B) / 3);
+                    int x3 = (int)((c3.R + c3.G + c3.B) / 3);
+                    int x4 = (int)((c4.R + c4.G + c4.B) / 3);
+                    int x5 = (int)((c5.R + c5.G + c5.B) / 3);
+                    int x6 = (int)((c6.R + c6.G + c6.B) / 3);
+                    int x7 = (int)((c7.R + c7.G + c7.B) / 3);
+                    int x8 = (int)((c8.R + c8.G + c8.B) / 3);
+                    int x9 = (int)((c9.R + c9.G + c9.B) / 3);
+
+                    int xb = (int)(a[1] * x1 + a[2] * x2 + a[3] * x3 + a[4] * x4 + a[5] * x5 + a[6] * x6 + a[7] * x7 + a[8] * x8 + a[9] * x9);
+                    if (xb < 0) xb = 0;
+                    if (xb > 255) xb = 255;
+
+                    Color newColor = Color.FromArgb(xb, xb, xb);
+                    objek8.SetPixel(x, y, newColor);
+                }
+            }
+            pictureBox2.Image = objek8;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Yakin ?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
